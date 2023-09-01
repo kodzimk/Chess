43f464f0066b9d5b02db8ac93ca3824eca969934
nurKaisar <proggamedev@gmail.com>
2023-09-01 16:29:54 +0600

    adding logic for pawn and starting adding to other piece

--- Pawn.h
+++ Pawn.h
@@ -22,5 +22,19 @@ public:
 		
 	}
 
+	void whiteMove(Vector2i mousPos,bool &clicked)
+	{
+		if (sprite.getGlobalBounds().contains(mousPos.x, mousPos.y))
+		{
+			clicked = true;
+		}
+	}
 
+	void blackMove(Vector2i mousPos, bool& clicked)
+	{
+		if (blackPawnS.getGlobalBounds().contains(mousPos.x, mousPos.y))
+		{
+			clicked = true;
+		}
+	}
 };
--- mainLoop.h
+++ mainLoop.h
@@ -17,7 +17,10 @@ public:
 	Map board;
 	Bishop whiteBishopF, whiteBishopS;
 	Bishop blackBishopF, blackBishopS;
+	Sprite sprite[32];
+
 	int Biindex = 0, WIndex = 0;
+
 	King blackKing;
 	King whiteKing;
 	RenderWindow window;
@@ -33,17 +36,31 @@ public:
 	Knight blackLnightS;
 	Queen blackQueen,whiteQueen;
 
+	bool ClickedToPiece = false;
+
+	Pawn* refWhitePawn;
+	Pawn* refBlackP;
+	Queen* refBlackQ;
+	Queen* refWhiteQ;
+	Knight* refBlackKnght;
+	Knight* refWhiteKnight;
+	Rook* refWhitRook;
+	Rook* refBlackRook;
+	King* refWhiteKing;
+	King* refBlackKing;
+	Bishop* refWhiteBishop;
+	Bishop* refBlackBishop;
+
+
 
 	int wrookIndex = 0,brookIndex = 0;
 	int whiteIndex = 0,blackIndex = 0,wBK = 0,wWK=0;
+	int width = 0, height = 200;
+	int clickX, clickY = 0;
 	Texture texture;
-	Sprite sprite;
-	
 
 	MainLoop()
-	{
-		texture.loadFromFile("NebunNegru.png");
-		sprite.setTexture(texture);
+	{	
 		init();
 		window.create(VideoMode(800, 800), "Chess Game");
 		Loop();
@@ -51,6 +68,22 @@ public:
 
 	void init()
 	{
+		texture.loadFromFile("board.png");
+		for (int i = 0; i < 32; i++)
+		{
+			if (i > 0 && i % 8 == 0)
+			{
+				height += 100;
+				width = 0;
+			}
+			if (i % 8 != 0)
+				width += 100;
+
+			sprite[i].setTexture(texture);
+			sprite[i].setTextureRect(IntRect(width, height, 100, 100));
+			sprite[i].setPosition(width, height);
+
+		}
 		for (int i = 0; i < 8; i++)
 		{
 			for (int j = 0; j < 8; j++)
@@ -145,6 +178,10 @@ public:
 	{
 		window.clear();
 		window.draw(board.boardS);
+		for (int i = 0; i < 32; i++)
+		{
+			window.draw(sprite[i]);
+		}
 		window.draw(blackKing.blackQuS);
 		window.draw(whiterookS.rookS);
 		window.draw(whiteRookF.rookS);
@@ -170,18 +207,77 @@ public:
 		window.display();
 	}
 
+	void CheckBoard()
+	{
+		int i = (int(clickY / 100 ));
+		int j = (int(clickX / 100));
+		if (refWhitePawn != NULL)
+		{
+			if (board.map[i][j] == 0)
+			{
+				if (i - refWhitePawn->sprite.getPosition().y / 100 <= 2)
+				{
+					(refWhitePawn)->sprite.setPosition(j * 100, i * 100);
+					board.map[i][j] = 1;
+				}
+			}
+		}
+		if (refBlackP != NULL)
+		{
+			if (board.map[i][j] == 0)
+			{
+				if (refBlackP->blackPawnS.getPosition().y / 100  - i<= 2)
+				{
+					(refWhitePawn)->blackPawnS.setPosition(j * 100, i * 100);
+					board.map[i][j] = -1;
+				}
+			}
+		}
+	}
+
 	void Loop()
 	{
 		while (window.isOpen())
 		{
 			Event event;
+			Vector2i mousePos = Mouse::getPosition(window);
 
 			while (window.pollEvent(event))
 			{
 				if (event.type == sf::Event::Closed)
 					window.close();
 			}
+			if (event.type == Event::MouseButtonPressed)
+			{
+				for (int i = 0; i < 8; i++)
+				{
+					if (whitePawns[i].sprite.getGlobalBounds().contains(mousePos.x, mousePos.y))
+					{
+						refWhitePawn = &whitePawns[i];
+						ClickedToPiece = true;
+					}
+					if (blacksPawn[i].blackPawnS.getGlobalBounds().contains(mousePos.x, mousePos.y))
+					{
+						refBlackP = &blacksPawn[i];
+						ClickedToPiece = true;
+					}
+				}
 
+				for (int i = 0; i < 32; i++)
+				{
+					if (sprite[i].getGlobalBounds().contains(mousePos.x, mousePos.y))
+					{
+						if (ClickedToPiece == true)
+						{
+							clickX = (int)sprite[i].getPosition().x;
+							clickY = (int)sprite[i].getPosition().y;
+							CheckBoard();
+							break;
+						}
+				    }
+				}
+			}
+			
 			Show();
 		}
 	}
